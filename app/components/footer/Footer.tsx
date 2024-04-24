9c136451947773feddaf45c52afd47285c3c4db2
Prashant8Khatiwada <prashantkhatiwada554@gmail.com>
2024-04-24 10:06:51 +0545

    responsiveness of footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -7,17 +7,23 @@ const Footer = () => {
   return (
     <div>
       <NewsLetter />
-      <div className="mt-[-90px] w-screen h-[577px] bg-color-secondary text-color-primary absolute flex items-center justify-center">
-        <div className="flex gap-36 mt-10">
+      <div className="mt-[-90px] w-screen h-[570px] sml:h-[1052px]  bg-color-secondary text-color-primary absolute flex items-center justify-center sml:justify-start">
+        <div className="grid grid-flow-col gap-36 lrg:gap-10 mt-10 sml:mt-20 px-8 sml:grid-flow-row">
           <div>
-            <Image src={Logo} alt="Logo" width={234} height={45.61} />
-            <p>copyright © travellian 2022 All rights reserved</p>
+            <Image
+              className="cursor-pointer w-[234px] mid:w-[200px]  "
+              src={Logo}
+              alt="Logo"
+            />
+            <p className="text-font-sm mid:text-[16px]">
+              copyright © travellian 2022 All rights reserved
+            </p>
           </div>
           <div>
-            <h2 className="text-font-md">
+            <h2 className="text-font-md mid:text-font-sm">
               <b>Menu</b>
             </h2>
-            <ul className="text-font-sm">
+            <ul className="text-font-sm mid:text-[16px]">
               <li>Home</li>
               <li>Explore</li>
               <li>Travel</li>
@@ -26,10 +32,10 @@ const Footer = () => {
             </ul>
           </div>
           <div>
-            <h2 className="text-font-md">
+            <h2 className="text-font-md mid:text-font-sm">
               <b>Information</b>
             </h2>
-            <ul className="text-font-sm">
+            <ul className="text-font-sm mid:text-[16px]">
               <li>Destination</li>
               <li>Supports</li>
               <li>Terms and Conditions</li>
@@ -37,20 +43,20 @@ const Footer = () => {
             </ul>
           </div>
           <div>
-            <h2 className="text-font-md">
+            <h2 className="text-font-md mid:text-font-sm">
               <b>Contact Info</b>
             </h2>
-            <ul className="text-font-sm">
+            <ul className="text-font-sm mid:text-[16px]">
               <li>+123 456 789</li>
               <li>info@travellian.com</li>
               <li>125, New Yourk, USA</li>
             </ul>
           </div>
           <div>
-            <h2 className="text-font-md">
+            <h2 className="text-font-md mid:text-font-sm">
               <b>Follow Us</b>
             </h2>
-            <ul className="text-font-sm">
+            <ul className="text-font-sm mid:text-[16px]">
               <li>+123 456 789</li>
               <li>info@travellian.com</li>
               <li>125, New Yourk, USA</li>
--- app/components/footer/NewsLetter.tsx
+++ app/components/footer/NewsLetter.tsx
@@ -3,21 +3,21 @@ import React from "react";
 const NewsLetter = () => {
   return (
     <div className="w-screen flex justify-center items-center ">
-      <div className="w-[62%] h-[180px] flex justify-around items-center border-[2px] border-black border-opacity-20  rounded-[12px] relative z-10 bg-color-primary">
-        <h3 className="text-font-lg text-color-secondary-light">
+      <div className="w-[62%] h-[180px] flex sml:flex-col px-[30px] gap-4 justify-between lrg:justify-normal   items-center border-[2px] border-black border-opacity-20  rounded-[12px] relative z-10 bg-color-primary">
+        <h3 className="text-[64px] text-color-secondary-light lrg:text-[43px] ">
           Our Newsletter
         </h3>
 
-        <div className="flex gap-4">
+        <div className="flex gap-4 lrg:gap-2  w-[50%] sml:w-[100%] ">
           <div className="flex flex-col gap-2">
             <label htmlFor="Email">Email</label>
             <input
-              className="px-[24px] py-[12px] bg-[#FFE8D4] rounded-md border-none text-color-secondary-light"
+              className="px-[24px] py-[12px]  lrg:px-[16px] lrg:py-[8px] mid:w-[80%]  bg-[#FFE8D4] rounded-md border-none text-color-secondary-light"
               type="text"
               placeholder="Enter your email"
             />
           </div>
-          <button className="bg-color-btn-primary rounded-md px-4 py-3 text-color-primary self-end">
+          <button className="bg-color-btn-primary rounded-md px-4 py-3 lrg:px-3 lrg:py-2 text-color-primary  self-end">
             Suscribe
           </button>
         </div>
